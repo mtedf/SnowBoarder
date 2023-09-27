972004acf5e6fffac1e4edbf25691c85bd7f2b4e
mtedf <tedfor485@morrisville.edu>
2023-09-27 13:01:09 -0400

    Triggering Particle Effects

--- Snow Boarder/Assets/New Folder/FinishLine.cs
+++ Snow Boarder/Assets/New Folder/FinishLine.cs
@@ -6,14 +6,15 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float fltLoadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            
+            finishEffect.Play();
             Invoke("ReloadScene", fltLoadDelay);
-        }
+        }   
         
     }
 
