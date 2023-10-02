152033113335eef1aabc26776c4d31b0562097b1
mtedf <tedfor485@morrisville.edu>
2023-10-02 12:58:19 -0400

    Prevent Double Play

--- Snow Boarder/Assets/New Folder/CrashDetector.cs
+++ Snow Boarder/Assets/New Folder/CrashDetector.cs
@@ -8,12 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float fltLoadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && hasCrashed == false)
         {
+            hasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
